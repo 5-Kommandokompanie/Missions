751c79d2cee23b42984348348d8d52ffb7776ba3
Jan Wöde <master@exclusiv-altis-life.de>
2023-02-25 22:55:00 +0100

    Neue Mission vom Freitag hinzugefügt

---
+++ Tanoa/24022023.Tanoa/initPlayerLocal.sqf
@@ -0,0 +1,4 @@
+//initPlayerLocal.sqf
+
+//PreLoad arsenal so as to stop loading screen on first open
+[ "Preload" ] spawn BIS_fnc_arsenal;
--- tools/getMissions.bat
+++ tools/getMissions.bat
@@ -8,12 +8,14 @@ set map2=Chernarus\260120.Chernarus_Winter
 set map3=Lythium\081800Anov20.lythium
 set map4=RHSPKL\211800Afeb21.rhspkl
 set map5=Tanoa\11022023.Tanoa
+set map6=Tanoa\24022023.Tanoa
 
 robocopy %source%\%map1% %dest%\%map1% /E
 robocopy %source%\%map2% %dest%\%map2% /E
 robocopy %source%\%map3% %dest%\%map3% /E
 robocopy %source%\%map4% %dest%\%map4% /E
 robocopy %source%\%map5% %dest%\%map5% /E
+robocopy %source%\%map6% %dest%\%map6% /E
 
 PING -n 2 127.0.0.1>nul
 
